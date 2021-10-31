0bcf9aa88e647f4ae2ae7026a55c7dccdd684cd2
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-10-31 15:10:19 -0400

    making sure the repo is up to date, getting rid of this computer

--- pages/components/LinkList/LinkList.js
+++ pages/components/LinkList/LinkList.js
@@ -3,7 +3,7 @@ import styles from "../../../styles/LinkList.module.css";
 const LinkList = ({ generatedLinks, lastFormatTime }) => {
     return (
         <div className={styles.generatedContent}>
-            { generatedLinks.length > 0 && (
+            { generatedLinks?.length > 0 && (
                 <>
                     <p><strong>{generatedLinks.length}</strong>&nbsp; Links Formatted</p>
                     <span className={styles.lastFormatSpan}>Last format: &nbsp;{lastFormatTime}</span>
@@ -12,7 +12,7 @@ const LinkList = ({ generatedLinks, lastFormatTime }) => {
             )}
 
             <span className={styles.list}>
-                {generatedLinks.map((generatedLink) =>
+                {generatedLinks?.map((generatedLink) =>
                     <div className={styles.formattedLinkItem}>
                         <img src={generatedLink} className={styles.formattedLinkImage} alt="Formatted Link Image" />
                         <p className={styles.generatedLink} >{generatedLink}</p>
